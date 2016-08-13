62b8530af919710d40e3c7f43277ffca6932949f
stratos-pc\stratos <straris88@gmail.com>
2016-08-13 16:56:18 +0200

    add connect to db

---
+++ calls/connect_to_mysql.php
@@ -0,0 +1,5 @@
+<?php 
+ // place your DB_host, UserName, Password, and DB_Name below where shown
+ mysql_connect("83.212.116.204","kokos","chinchilla321") or die ("Could not connect.");
+ mysql_select_db("testSite") or die ("no database");
+ ?> 
